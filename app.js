a7fb2ccdda2422f165ec506ce5b6f69ff6fe4ca4
Kervintz Noel <kervintz84@hotmail.com>
2021-03-29 00:39:57 -0400

    add dotenv modules

--- app.js
+++ app.js
@@ -3,32 +3,10 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const nodemailer = require('nodemailer');
 const port = 3003
+require('dotenv').config();
 
 
 const app = express();
-
-const transporter = nodemailer.createTransport({
-    service: "hotmail",
-    auth: {
-        user: "kervdev@outlook.com",
-        pass: "Dream#Dev23!"
-    }
-});
-
-const options = {
-    from: "kervdev@outlook.com",
-    to: "kervcodes@gmail.com",
-    subject: "Sending email with node.js",
-    text: "Work or did not work!"
-};
-
-transporter.sendMail(options, (err, info) => {
-    if(err) {
-        console.log(err);
-        return; 
-    }
-    console.log("Sent: " + info.response)
-})
 
 app.use(bodyParser.json());
 
@@ -38,10 +16,35 @@ app.get('/api', (req, res, next) => {
   res.send('API status is up');
 })
 
+console.log(process.env);
 
 app.post('/api/contact', (req, res, next) => {
 
-    console.log(req.body);
+    // console.log(req.body);
+    console.log(process.env);
+
+    const transporter = nodemailer.createTransport({
+        service: "hotmail",
+        auth: {
+            user: "kervdev@outlook.com",
+            pass: process.env.NODE_MAILER_BYPASS
+        }
+    });
+    
+    const options = {
+        from: "kervdev@outlook.com",
+        to: "kervcodes@gmail.com",
+        subject: req.body.subject,
+        text: "email From: " + req.body.email + " message: " + req.body.message,
+    };
+    
+    transporter.sendMail(options, (err, info) => {
+        if(err) {
+            console.log(err);
+            return; 
+        }
+        console.log("Sent: " + info.response)
+    })
 
    
 });
