45d1c92e6c3534589c082176259495d2c869009e
Kervintz Noel <kervintz84@hotmail.com>
2021-06-26 21:25:33 -0400

    Unable to send email using nodemailer and gmail

--- app.js
+++ app.js
@@ -2,45 +2,83 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const nodemailer = require('nodemailer');
-const port = process.env.PORT || 3003;
-// require('dotenv').config();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.use(cors())
 
-app.post("/send_mail", cors(), async (req, res) => {
+
+app.get('/data', async (req, res, next) => {
+  res.send("test")
+})
+
+app.post("/data", cors(), async (req, res, next) => {
     let text = req.body;
+
+    let { name, email, subject, message } = req.body;
+
+    console.log("email: ", email);
     
-    const transport = nodemailer.createTransport({
-        host : process.env.MAIL_HOST,
-        port: process.env.MAIL_PORT,
-        auth: {
-            user: process.env.MAIL_USER,
-            pass: process.env.MAIL_PASS
+    const transporter = nodemailer.createTransport({
+      service: "imap.gmail.com",
+      port: 993,
+      auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASS
+      }
+      });
+
+
+      const mailOptions = {
+        from: email, 
+        to: process.env.EMAIL, 
+        subject: subject, 
+        html: '<h1>this is a test mail.</h1>'
+      }
+
+      // transporter.verify(function(error, success) {
+      //   if (error) {
+      //     console.log(error);
+      //   } else {
+      //     console.log("Server is ready to take our messages");
+      //   }
+      // });
+    
+    transporter.sendMail(mailOptions,  function(err, data){
+        if (err) {
+          console.log(err)
+        } else {
+          console.log("success:", data)
         }
     })
-    
-    await transport.sendMail({
-        from: process.env.MAIL_FROM,
-        to: "kervcodes@outlook.com",
-        subject: "test email",
-        html: `<div className="email" style="
-        border: 1px solid black;
-        padding: 20px;
-        font-family: sans-serif;
-        line-height: 2;
-        font-size: 20px;
-        ">
-        <h2>Here is your email!</h2>
-        <p>${text}</p>
-        </div>
-        `
-    })
+      
+      
+      
+      
+    //   function(error, info) {
+    //     from: process.env.EMAIL,
+    //     subject: subject,
+
+    //     html: `<div className="email" style="
+    //     border: 1px solid black;
+    //     padding: 20px;
+    //     font-family: sans-serif;
+    //     line-height: 2;
+    //     font-size: 20px;
+    //     ">
+    //     <h2>Here is your email from ${name}!</h2>
+    //     <p>${message}</p>
+    //     </div>
+    //     `
+    // })
 } )
 
 
